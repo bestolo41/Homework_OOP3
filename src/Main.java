0d2ec5d3513fcf31ce4b7eef2bc32b38c89c7213
BULAT <ibragimovbi@yandex.ru>
2022-10-31 20:43:01 +0300

    ДЗ про транспорт

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import transport.Bus;
+import transport.Train;
+import transport.Transport;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Transport lastochka = new Train("Ласточка", "В-901", 2011, "Россия",
+                "", 301, 3500, "", "Белорусский вокзал",
+                "Минск-Пассажирский",
+                11);
+
+        Transport leningrad = new Train("Ленинград", "D-125", 2019, "Россия",
+                "", 270, 1700, "", "Ленинградский вокзал",
+                "Ленинград-Пассажирский", 8);
+
+        System.out.println(lastochka);
+        System.out.println(leningrad);
+
+        Transport bus1 = new Bus("Mersedes", "Sprinter", "Бензин", 2015, "Germany", "White", 130);
+        Transport bus2 = new Bus("Ford", "Transit", "Дизель", 2017, "Russia", "Blue", 120);
+        Transport bus3 = new Bus("GAZ", "Gazelle", "Бензин",2019, "Russia", "Yellow", 100);
+
+        System.out.println(bus1);
+        System.out.println(bus2);
+        System.out.println(bus3);
+
+        bus1.refill();
+        bus2.refill();
+        bus3.refill();
     }
 }
